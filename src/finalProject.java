b0d7cca9c240c0e77c4551d01d3ff174f9a1fd23
DESKTOP-249BUD4\Owner <brandonfung17@gmail.com>
2022-04-24 16:42:16 -0600

    Add functions

---
+++ src/finalProject.java
@@ -0,0 +1,145 @@
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+class finalProject {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Connection function to 
+     * @param port
+     * @param database
+     * @param password
+     * @return
+     */
+    public static Connection makeConnection(String port, String database, String password) {
+        try {
+            Connection conn = null;
+            conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:" + port+ "/" + database+
+						"?verifyServerCertificate=false&useSSL=true", "msandbox",
+						password);
+				// Do something with the Connection
+				System.out.println("Database " + database +" connection succeeded!");
+				System.out.println();
+				return conn;
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        }
+        return null;
+    }
+
+    /**
+     * Run query function
+     * @param conn
+     */
+    public static void runQuery(Connection conn) {
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM Item;");
+            // Now do something with the ResultSet ....
+            
+            rs.beforeFirst();
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) 
+                        + ":" + rs.getString(2) 
+                        + ":" + rs.getString(3) 
+                        + ":" + rs.getString(4));
+            }
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            // it is a good idea to release resources in a finally{} block
+            // in reverse-order of their creation if they are no-longer needed
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                stmt = null;
+            }
+        }
+    }
+
+    public static void createClass() {
+
+    }
+
+    public static String[] listClasses() {
+        return null;
+    }
+
+    public static void activateClass() {
+
+    }
+
+    public static String[] showActiveClass() {
+        return null;
+    }
+
+    public static String[] showCategories() {
+        return null;
+    }
+
+    public static void addCategory() {
+
+    }
+
+    public static String[] showAssignment() {
+        return null;
+    }
+
+    public static void addAssignment() {
+
+    }
+
+    public static void addStudent() {
+
+    }
+
+    public static void editStudent() {
+
+    }
+
+    public static String[] showStudents(String key) {
+        return null;
+    }
+
+    public static void gradeAssignment() {
+
+    }
+
+    public static void showStudentsGrades() {
+
+    }
+
+    public static String[] gradebook() {
+        return null;
+    }
+
+}
