3f1bd5216fb9a97c383cde2508e2ed245db6c5d0
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2022-04-30 16:07:18 -0600

    testing

--- src/Connector J 8.0/finalProject.java
+++ src/Connector J 8.0/finalProject.java
@@ -899,9 +899,10 @@ class finalProject {
 
             System.out.println("HERE");
             rs = ps.getResultSet();
-
-            if(rs.getInt(1) < Integer.parseInt(grade)){
-                System.out.println("WARNING: The grade you are trying to input exceed the number of points configured (" + rs.getInt(1) + ").");
+            while(rs.next()) {
+                if (rs.getInt(1) < Integer.parseInt(grade)) {
+                    System.out.println("WARNING: The grade you are trying to input exceed the number of points configured (" + rs.getInt(1) + ").");
+                }
             }
 
             /*
