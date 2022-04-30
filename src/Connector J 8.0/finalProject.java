7606a805172966768ae49761dcf611e6c0de5cce
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2022-04-30 15:24:24 -0600

    testing

--- src/Connector J 8.0/finalProject.java
+++ src/Connector J 8.0/finalProject.java
@@ -814,7 +814,7 @@ class finalProject {
                 rs = ps.getResultSet();
                 //rs.beforeFirst();
                 while (rs.next()) {
-                    System.out.println(rs.getInt(1) + " , " + rs.getString(2) + " , " + rs.getString(3) + " , " + rs.getString(4) + ", " + rs.getInt(5));
+                    System.out.println(rs.getInt(1) + " , " + rs.getString(2) + " , " + rs.getString(3) + " , " + rs.getString(4) + ", " + rs.getDouble(5));
                 }
 
             }
