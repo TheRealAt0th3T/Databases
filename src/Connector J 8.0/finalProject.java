f5b15a665c1bfafb11a96a3455cb9116b5c4fa2c
DESKTOP-249BUD4\Owner <brandonfung17@gmail.com>
2022-04-30 15:09:02 -0600

    asdf

--- src/Connector J 8.0/finalProject.java
+++ src/Connector J 8.0/finalProject.java
@@ -723,7 +723,7 @@ class finalProject {
             }
             
 
-            stmt2 = conn.prepareStatement("SELECT * FROM students WHERE students_username = ?");
+            stmt2 = conn.prepareStatement("SELECT * FROM students WHERE students_username = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             stmt2.setString(1, username);
             boolean hasResult = stmt2.execute();
             System.out.println("penis");
@@ -735,7 +735,7 @@ class finalProject {
             }
 
             if(hasResult && rs.next()){ //therefore student exists
-                stmt3 = conn.prepareStatement("UPDATE students SET class_id = ? WHERE username = ?");
+                stmt3 = conn.prepareStatement("UPDATE students SET class_id = ? WHERE username = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
                 stmt3.setInt(1, temp);
                 stmt3.setString(2, username);
                 stmt3.execute();
